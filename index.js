cbf15e8d35d0d446511935c8776c8d70829e3b8a
Tarakant2002 <140203128+Tarakant2002@users.noreply.github.com>
2023-08-28 16:41:02 +0530

    Portfolio website by Tarakant

---
+++ index.js
@@ -0,0 +1,15 @@
+function home(){
+    window.location.href='index.html'
+        }
+        function about(){
+    window.location.href='about.html'
+        }
+        function work(){
+    window.location.href='work.html'
+        }
+        function service(){
+    window.location.href='service.html'
+        }
+
+        //for change name
+       
